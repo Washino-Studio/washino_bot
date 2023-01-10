3a0ef6794984679737afea5a35a3b1b5d923b5b2
//webnono// <91453043+webnono8@users.noreply.github.com>
2023-01-10 21:57:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,25 @@
 
 //-----Library-----//
 
-const Discord = require("discord.js")
-const intents = new Discord.IntentsBitField(3276799)
-const bot = new Discord.Client({intents})
-const token = process.env.token
-const loadCommands = require("./Loaders/loadCommands")
-const loadEvents = require("./Loaders/loadEvents")
-const config = require("./config")
-const loadSlashCommands = require("./Loaders/loadSlashCommands")
+const Discord = require("discord.js");
+const intents = new Discord.IntentsBitField(3276799);
+const bot = new Discord.Client({intents});
+const token = process.env.token;
+const loadCommands = require("./Loaders/loadCommands");
+const loadEvents = require("./Loaders/loadEvents");
+const config = require("./config");
+const loadSlashCommands = require("./Loaders/loadSlashCommands");
 
 
 
 //----Variables----//
 
-bot.commands = new Discord.Collection()
+bot.commands = new Discord.Collection();
 
 //--Import--//
-loadCommands(bot)
-loadEvents(bot)
-loadSlashCommands(bot)
+loadCommands(bot);
+loadEvents(bot);
+loadSlashCommands(bot);
 
 
-bot.login(token)
+bot.login(token);
